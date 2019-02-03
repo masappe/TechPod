45dff85d54c935cf835aa6fbd8a0eded1f59c79a
早川雅人 <masato@hayakawamasatonomacbook-pro.local>
2019-02-03 11:32:44 +0900

    Initial Commit

---
+++ TechPod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechPod
+//
+//  Created by 早川雅人 on 2019/02/03.
+//  Copyright © 2019 早川雅人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
